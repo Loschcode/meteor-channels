adec26749fd8de8acfd6ff48d42a430ba98fb981
Loschcode <bonjour@laurentschaffner.com>
2016-04-15 08:39:04 +0200

    angular form binding

--- client/app.ts
+++ client/app.ts
@@ -4,12 +4,13 @@ import {NgZone, Component} from 'angular2/core';
 import {bootstrap} from 'angular2-meteor-auto-bootstrap';
 import {Parties} from '../collections/parties';
 import {Tracker} from 'meteor/tracker';
-
 import {Mongo} from 'meteor/mongo';
+import {PartiesForm} from './imports/parties-form/parties-form';
  
 @Component({
   selector: 'app',
-  templateUrl: 'client/app.html'
+  templateUrl: 'client/app.html',
+  directives: [PartiesForm]
 })
 
 class Socially {
@@ -18,6 +19,11 @@ class Socially {
   constructor () {
     this.parties = Parties.find();
   }
+  
+  removeParty(party) {
+    Parties.remove(party._id);
+  }
+
 }
  
 bootstrap(Socially);
